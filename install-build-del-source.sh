774f5cb8d39fe0c80a5dc6d06dc6686eff225164
Aur Adani <ofirg@moveo.co.il>
2019-02-26 11:42:09 +0200

    demo

--- install-build-del-source.sh
+++ install-build-del-source.sh
@@ -1,6 +1,6 @@
 npm install
 echo 'install done'
 
-rm -rf dist && npm run build && rm -rf src && mkdir src && touch src/dummy.ts
+npm run build && rm -rf src && mkdir src && touch src/dummy.ts
 echo 'build done'
 echo 'del source done'
