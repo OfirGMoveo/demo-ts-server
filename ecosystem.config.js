006e4bdea1be304cbb6bc72e62d4e312285d4e9e
Aur Adani <ofirg@moveo.co.il>
2019-02-26 12:21:00 +0200

    demo

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -25,7 +25,7 @@ module.exports = {
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     args: 'one two',
     instances: 'max',
-    instances: 'cluster',
+    exec_mode: 'cluster',
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
