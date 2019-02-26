d32c5f444e2d3dd1d3076298e2a3f1fb94da948f
Aur Adani <ofirg@moveo.co.il>
2019-02-26 11:05:19 +0200

    init commit

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,6 @@
 import * as express from 'express'
-// import * as bodyParser from 'body-parser'
-
 console.log('in index.js');
-
 const port = process.env.PORT || 3000;
-
 const app = express();
-
-// app.use(bodyParser.json());
-
 app.get('/', (req, res) => res.status(200).send(`hello from test server, im on port ${port}`));
-
 app.listen(port, () => console.log(`up on port ${port}`));
